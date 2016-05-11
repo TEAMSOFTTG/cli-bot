ba458817aff233832f04c725182888e05d1fccd0
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-11 14:36:54 +0430

    Update WaderTGbot.lua

--- bot/WaderTGbot.lua
+++ bot/WaderTGbot.lua
@@ -249,19 +249,20 @@ function create_config( )
 	"voice",
 	"wai"
     },
-    sudo_users = {67559249,71951412,154868817,143531868,0,tonumber(our_id)},--Sudo users
+    sudo_users = {147237496,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[WaderTG v4
+    about_text = [[Soft TG
 An advanced administration bot based on TG-CLI written in Lua
 
 Admins
+@MohammadNBG
 @mohammadsdi4799
 @iphonei
 @Amin1779
 @Oo_hamed_ice_fuckeram_oO
 
 Our channels
-@WaderTGTeam
+@SoftTG_TEAM
 
 thankyou for all admins bot WaderTG
 ]],
